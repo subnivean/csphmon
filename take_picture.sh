c6d9e1c4d82921aed8c8dfa31c4ffce301b68693
Mark Krautheim <mark.krautheim@clowderhill.com>
2017-12-23 20:33:37 -0500

    Separate command lines

--- take_picture.sh
+++ take_picture.sh
@@ -2,8 +2,11 @@
 
 # Take a picture with the Raspi Camera. Make sure to turn on the overhead light first!
 
-raspistill -o foo.jpg -t 1 \
-   && ~/github/Dropbox-Uploader/dropbox_uploader.sh upload \
-        foo.jpg \
-        bar_$(echo $(date --iso-8601=seconds)|cut -d'-' -f1-3).jpg \
-   && rm -f foo.jpg
+filename=bar_$(echo $(date --iso-8601=seconds)|cut -d'-' -f1-3).jpg
+
+#raspistill --ISO 3200 --shutter 8000000 -o foo.jpg -t 1 --exposure verylong --awb incandescent
+raspistill -o foo.jpg -t 1 
+
+/home/pi/github/Dropbox-Uploader/dropbox_uploader.sh upload foo.jpg $filename
+
+rm -f foo.jpg
